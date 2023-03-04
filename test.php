6cf2509dec86615ca1d7824bedbf02f950f0be3f
Ivan Lokh <ivanlokh@gmail.com>
2023-03-04 17:21:13 +0200

    Start

---
+++ test.php
@@ -0,0 +1,18 @@
+<?php
+
+$name = "Alex";
+$Name = "Cris";
+$NAME = "Elene";
+
+$my_variable = "";
+$myVariable = "";
+
+
+
+// Так не потрібно робити, кирилиця не допустима!!!
+$Алекс = "Б"; 
+echo $NAME;
+
+
+
+?>
