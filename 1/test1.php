535715e991ee69fef45e4f9f886060a466352dc8
Ivan Lokh <ivanlokh@gmail.com>
2023-03-05 17:26:52 +0200

    _

--- 1/test1.php
+++ 1/test1.php
@@ -7,18 +7,10 @@ $NAME = "Elene";
 $my_variable = "";
 $myVariable = "";
 
-
-
 // Так не потрібно робити, кирилиця не допустима!!!
 $Алекс = "Б"; 
 
-
-
 echo "My name is: $Name <br>"; //Або так!
 echo 'My name is:' . $name; //Або так!
-
-
-
-
 
 ?>
